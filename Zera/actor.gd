f30cf30e3306822a9e56833b39c0ccdbd2dd2d4c
nadlereece <nadler2122@my.uwstout.edu>
2021-03-03 17:32:32 -0600

    MovementExample
    Shows character movement on a ground plane

---
+++ Zera/actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name actor
+
+# variables
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed:  = Vector2(250, 950)
+export var gravity: = 450.0
+var velocity: = Vector2.ZERO
+
