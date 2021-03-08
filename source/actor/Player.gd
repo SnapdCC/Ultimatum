999f3ebdba79a15efd158d6e85474ed516c0205b
nadlereece <nadler2122@my.uwstout.edu>
2021-03-08 17:06:23 -0600

    Placeholder Code plus Comments
    Created a system in which the player can now be kept track of whether they're facing right or left so the attack can appear on the respective side they're facing, plus framework for the punch, kick, and guard code for when I figure that out

--- source/actor/Player.gd
+++ source/actor/Player.gd
@@ -1,5 +1,10 @@
 extends actor
 
+# variables
+var face_right = true
+var face_left = false
+var holder = true
+
 # these inputs cause movement when pressed
 func _physics_process(delta: float) -> void:
 	var direction = Vector2(
@@ -8,5 +13,22 @@ func _physics_process(delta: float) -> void:
 		# down is default 1, when both are pressed simutaniously no movement, when up is pressed -1 is registered
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
+	
+	# checks to see if the player is facing right or left
+	if Input.is_action_just_released("move_right"):
+		face_right = true
+		face_left = false
+	elif Input.is_action_just_released("move_left"):
+		face_left = true
+		face_right = false
+	
+	# eventual placement of code to make punch, kick, and guard "animations" appear
+	if Input.is_action_pressed("punch"):
+		holder = true
+	elif Input.is_action_pressed("kick"):
+		holder = true
+	elif Input.is_action_pressed("guard"):
+		holder = true
+	
 	# gives speed a value
 	velocity = speed * direction
