49a09cad65cb1d831579acb9286beebc1eb8db3b
nadlereece <nadler2122@my.uwstout.edu>
2021-03-08 16:35:37 -0600

    Comments
    Comments put for easier understanding

--- source/actor/Player.gd
+++ source/actor/Player.gd
@@ -1,8 +1,12 @@
 extends actor
 
+# these inputs cause movement when pressed
 func _physics_process(delta: float) -> void:
 	var direction = Vector2(
+		# right is default 1, when both are pressed simutaniously no movement, when left is pressed -1 is registered
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		# down is default 1, when both are pressed simutaniously no movement, when up is pressed -1 is registered
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
+	# gives speed a value
 	velocity = speed * direction
--- source/actor/actor.gd
+++ source/actor/actor.gd
@@ -6,5 +6,6 @@ export var speed:  = Vector2(250, 250)
 export var gravity = 0
 var velocity: = Vector2.ZERO
 
+# this makes the character move
 func _physics_process(delta: float) -> void:
 	velocity = move_and_slide(velocity)
