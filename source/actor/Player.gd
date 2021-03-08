d926811dccfb64400ecd28f51b5d286a7bb1f954
nadlereece <nadler2122@my.uwstout.edu>
2021-03-08 16:23:15 -0600

    Movement Part 2
    now with Up Down movement!

---
+++ source/actor/Player.gd
@@ -0,0 +1,8 @@
+extends actor
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+	velocity = speed * direction
---
+++ source/actor/actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name actor
+
+# variables
+export var speed:  = Vector2(250, 250)
+export var gravity = 0
+var velocity: = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity = move_and_slide(velocity)
