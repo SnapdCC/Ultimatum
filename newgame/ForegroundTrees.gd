34468da2adbf600b42346f1f12e055c9998ffbbc
GrabowskyS <71021100+GrabowskyS@users.noreply.github.com>
2021-03-29 16:18:48 -0500

    Added trees to level

---
+++ newgame/ForegroundTrees.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	modulate.a8 = 0.80
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
