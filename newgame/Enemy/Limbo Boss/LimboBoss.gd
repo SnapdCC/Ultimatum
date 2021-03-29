140b85f6da40c2e076f3e332147e3360e3c71a3c
nadlereece <nadler2122@my.uwstout.edu>
2021-03-29 17:02:32 -0500

    Limbo Boss In Level

---
+++ newgame/Enemy/Limbo Boss/LimboBoss.gd
@@ -0,0 +1,11 @@
+extends CollisionShape2D
+
+onready var animationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_HurtAreaLimboPunch_area_entered(area):
+	pass # Replace with function body.
