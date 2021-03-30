f0166fdc8128cfce4cd16aef9f87ec90c3c24122
nadlereece <nadler2122@my.uwstout.edu>
2021-03-30 17:45:42 -0500

    Limbo Boss Death

--- newgame/Enemy/Limbo Boss/LimboBoss.gd
+++ newgame/Enemy/Limbo Boss/LimboBoss.gd
@@ -1,13 +1,23 @@
-extends Navigation2D
+extends Node2D
 
 onready var animationPlayer = $AnimationPlayer
 onready var bossHealth = $"../CanvasLayer/BossHealth/HealthBar"
 var maxHealth = 200.0
 var health = 200.0
 
+var tempAnim
+var anim_new
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bossHealth.value = (health / maxHealth) * 100
+	tempAnim = "Idle"
+	animationSwapper(tempAnim)
+
+
+func animationSwapper(anim):
+	anim_new = anim
+	animationPlayer.play(anim)
 
 
 func _on_HurtAreaLimboPunch_area_entered(area):
@@ -18,6 +28,10 @@ func _on_LimboHitbox_area_entered(area):
 	pass # Replace with function body.
 
 func stunHit(damageTake, stunFrame):
-	health -= 3
+	health -= 5
 	print(health / maxHealth)
 	bossHealth.value = (health / maxHealth) * 100
+	
+
+	if (health == 0):
+		animationPlayer.play("Death")
