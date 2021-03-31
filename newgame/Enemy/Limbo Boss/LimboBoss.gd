363b8cce1fd1d0f236efeb6df9b6c6e6d492caba
nadlereece <nadler2122@my.uwstout.edu>
2021-03-30 19:37:09 -0500

    Walking Animation Fixed
    however, I can't get his death animation working properly

--- newgame/Enemy/Limbo Boss/LimboBoss.gd
+++ newgame/Enemy/Limbo Boss/LimboBoss.gd
@@ -9,6 +9,7 @@ var tempAnim
 var anim_new
 var isWalking = false
 var alive = true
+var dontLoop = false
 
 onready var playerPosition = get_parent().get_node("Player")
 var bossSpeed = 25
@@ -18,24 +19,29 @@ onready var bossPosition = get_parent().get_node("LimboBoss")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bossHealth.value = (health / maxHealth) * 100
-	if (!isWalking):
+	if (!isWalking and alive):
 		tempAnim = "Idle"
-	animationSwapper(tempAnim)
 
 func _physics_process(delta):
 	if(alive):
+		isWalking = true
 		if (playerPosition.position.x > position.x):
 			$Sprite.scale.x = -1
 		else:
 			$Sprite.scale.x = 1
-		isWalking = true
 		move_and_slide((playerPosition.position - (bossPosition.position - Vector2(0, 20))).normalized() * bossSpeed)
 		tempAnim = "Walk"
+	else:
+		if (dontLoop):
+			animationPlayer.stop()
+			animationPlayer.play("Death")
 
+	animationSwapper(tempAnim)
 
 func animationSwapper(anim):
 	anim_new = anim
 	animationPlayer.play(anim)
+	
 
 
 func _on_HurtAreaLimboPunch_area_entered(area):
@@ -49,9 +55,9 @@ func stunHit(damageTake, stunFrame):
 	health -= 5
 	print(health / maxHealth)
 	bossHealth.value = (health / maxHealth) * 100
-	
 
 	if (health <= 0):
-		animationPlayer.play("Death")
+		isWalking = false
 		alive  = false
+		dontLoop = true
 		
