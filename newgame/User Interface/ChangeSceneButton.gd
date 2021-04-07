ce944ce6f79fc2087a6cc817fadcfb1504002559
SnapdCC <nilesb9960@my.uwstout.edu>
2021-04-07 16:28:50 -0500

    Fixed Pause Menu

--- newgame/User Interface/ChangeSceneButton.gd
+++ newgame/User Interface/ChangeSceneButton.gd
@@ -5,6 +5,7 @@ export(String, FILE) var next_scene_path: = ""
 
 
 func _on_button_up():
+	get_tree().paused = false
 	get_tree().change_scene(next_scene_path)
 
 func _get_configuration_warning() -> String:
--- newgame/User Interface/Pause.gd
+++ newgame/User Interface/Pause.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	pause_mode=Node.PAUSE_MODE_PROCESS
+
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state:= not get_tree().paused
@@ -7,4 +10,6 @@ func _input(event):
 		visible = new_pause_state
 
 func _on_ResumeButton_button_up():
-	Input.action_press("pause")
+	var new_pause_state:= false
+	get_tree().paused = new_pause_state
+	visible = new_pause_state
